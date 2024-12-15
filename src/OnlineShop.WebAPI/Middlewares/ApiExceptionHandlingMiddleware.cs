5058fb0de18b26f427925796ab8fdd9e3043ff1d
Mehdi taherabadi <taherabadi@gamelectronics.com>
2024-12-15 22:05:08 +0330

    Add middlewares to handle exception

---
+++ src/OnlineShop.WebAPI/Middlewares/ApiExceptionHandlingMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace OnlineShop.WebAPI.Middlewares;
+
+public class ApiExceptionHandlingMiddleware
+{
+    #region constrctor
+
+    private readonly RequestDelegate _next;
+    //private readonly ILogger<ApiExceptionHandlingMiddleware> _logger;
+    public ApiExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    #endregion
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        var result = JsonSerializer.Serialize(ex.Message);
+        context.Response.ContentType = "application/json";
+        await context.Response.WriteAsync(result);
+    }
+}
--- src/OnlineShop.WebAPI/Program.cs
+++ src/OnlineShop.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using OnlineShop.IoC;
+using OnlineShop.WebAPI.Middlewares;
 using Swashbuckle;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,7 +21,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseMiddleware<ApiExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
