f7ded39f5f1bfa994446540c91aa84d200b3342c
Mehdi taherabadi <taherabadi@gamelectronics.com>
2024-12-15 17:30:53 +0330

    Add project files.

---
+++ src/OnlineShop.IoC/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnlineShop.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddIoC(this IServiceCollection services, IConfiguration configuration)
+    {
+        _IntilizeDbContext(services, configuration);
+        return services;
+    }
+    private static void _IntilizeDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddDbContext<ApplicationContext>(options =>
+        //                           options.UseSqlServer(
+        //                               configuration.GetConnectionString("DefaultConnection"),
+        //                               b => b.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+    }
+
+}
